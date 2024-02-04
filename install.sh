f2dd95417281fdb31fb25403be8654d98c4736bb
“cy-maxx” <cyber.mjp@gmail.com>
2024-02-04 15:27:14 +0000

    v0402_17.27

--- install.sh
+++ install.sh
@@ -2,10 +2,16 @@
 
 #!!!install version with pip!!!#
 sudo apt update
-sudo apt install python3-pip python3-venv
-
-mkdir cy-maxx
+sudo apt install python3-pip python3-venv -y
+#-----
+mkdir ~/cy-maxx
 cd cy-maxx
 python3 -m venv env_cy-maxx
 source env_cy-maxx/bin/activate
 pip install django
+#-----
+django-admin startproject app_cymaxx .
+python manage.py makemigrations     #for new migrations made
+python manage.py migrate
+python manage.py createsuperuser    #ubuntu/ubuntu
+python manage.py runserver 0.0.0.0:8000     #before run it you must complete with your AWS_Public_IP in Allow_host in settings.py
