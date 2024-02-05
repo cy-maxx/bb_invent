85e60296af717ba95af16cfc69d7d3736207755d
“cy-maxx” <cyber.mjp@gmail.com>
2024-02-05 19:06:46 +0000

    v0502_21.05

--- cy-maxx/recon_active/urls.py
+++ cy-maxx/recon_active/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("0", views.index),
-#    path("<int:var2>", views.dynamic_fct_by_nr),
-#    path("<str:var1>", views.dynamic_fct),
-    path("<str:month>", views.dynamic_fct_by_month)
+    path("",views.index)
+    # path("0", views.index),
+    # path("<int:var2>", views.dynamic_fct_by_nr),
+    # path("<str:var1>", views.dynamic_fct),
+    # path("<str:month>", views.dynamic_fct_by_month)
     ]
