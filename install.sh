06794c0a24424089c4e72918857caa9bc28662dd
cy-maxx <155469490+cy-maxx@users.noreply.github.com>
2024-03-02 17:48:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,6 +50,9 @@ sudo mv /root/go/bin/subfinder /usr/local/bin/
 vi /home/ubuntu/.config/subfinder/provider-config.yaml
 ##---configure the provider-config.yaml file with your APIs keys.
 ##how it works
+
+#### BurpSuite configuration ####
+git clone https://github.com/codewatchorg/Burp-UserAgent.git  //for modification of headers into BurpSuite
 subfinder -dL targets/../scope.txt -all -oD /recon -json subfinder_recon.json -cs
 
 
